dae74198a6f164670244043a269ed7f2dc7da12c
Wulandary14 <Moonywulan14@gmail.com>
2026-03-30 19:49:26 +0700

    perulangan dengan WHILE

--- Looping.cpp
+++ Looping.cpp
@@ -14,3 +14,15 @@ for(int i=0; i <= 4; i++){
 cout << endl;
 //untuk mendapatkanb urutan angka acak yg berbeda
 srand(time(0));
+
+//Perulangan dengan WHILE
+cout << "PERULANGANN WHILE" << endl;
+x = 1 + rand()%10;
+
+while(x<=5){
+cout << "Bilangan acak = " << x << endl;
+x = rand( )%10;
+}
+
+cout << "Bilangan acak while yang tearkhir =" << x << endl;
+cout << endl;
